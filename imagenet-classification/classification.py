1e9014590ed5f8d98c5a1b1420f2eb01b6dfc736
Naoki-Kato <g.ntkatoh@gmail.com>
2018-11-27 19:21:43 +0900

    remove commented out for debug

--- imagenet-classification/classification.py
+++ imagenet-classification/classification.py
@@ -112,9 +112,9 @@ def train():
         # 320x320 image inputs, the input image size of ResNet is set as
         # 224x224. We need to get tar file and create cache file(320x320 images).
         # Please check README.
-        # data = data_iterator_imagenet(
-        #     args.batch_size, args.train_cachefile_dir)
-        # vdata = data_iterator_imagenet(args.batch_size, args.val_cachefile_dir)
+        data = data_iterator_imagenet(
+            args.batch_size, args.train_cachefile_dir)
+        vdata = data_iterator_imagenet(args.batch_size, args.val_cachefile_dir)
         num_classes = 1000
     t_model = get_model(
         args, num_classes, test=False, tiny=args.tiny_mode)
